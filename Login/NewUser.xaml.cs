dbe12bd956a848149cb72ef72f81e386c1b68674
Shankar Anbalagan <bravoshankar@gmail.com>
2018-10-27 20:15:18 +0530

    Revert "Remove user function added"
    This reverts commit fa46a3412fa5fe0371d99069fede9074d3fc9422.

--- Login/NewUser.xaml.cs
+++ Login/NewUser.xaml.cs
@@ -34,8 +34,8 @@ namespace MockSAP
         {
             this.Show();
             status1.Text = "(Enter 10 or less digit number)";
-            status2.Text = "(Atmost 20 characters[LEAVE THIS FIELD BLANK IF REMOVING USER])";
-            status3.Text = "(Atmost 10 characters)";
+            status2.Text = "(Atmost 20 characters)";
+            status3.Text = "(Atmost 10)";
             status4.Text = "(Retype password)";
             adminLogin.Close();
             mainWindow.IsEnabled = false;
@@ -64,26 +64,6 @@ namespace MockSAP
             
         }
 
-        private void RemoveUser_Button_Click(object sender, RoutedEventArgs e)
-        {
-            status1.Text = "";
-            status3.Text = "";
-            status4.Text = "";
-
-            String uid, upass, upass_v;
-            uid = UserId.Text.Trim();
-            upass = UPassword.Password.ToString().Trim();
-            upass_v = VerifyUPassword.Password.ToString().Trim();
-
-            if (VerifyData(uid, upass, upass_v))
-            {
-                if (databaseConnection.removeUser(uid, upass))
-                    MessageBox.Show("User removed Successfully");
-                mainWindow.IsEnabled = true;
-                this.Close();
-            }
-        }
-
         private Boolean VerifyData(String uid, String uname, String upass, String upass_v)
         {
             if(uid.Length==0 || uname.Length == 0 || upass.Length == 0 || upass_v.Length == 0)
@@ -94,7 +74,7 @@ namespace MockSAP
                 status4.Foreground = new SolidColorBrush(Colors.Red);
                 status1.Text = "(Enter 10 or less digit number)";
                 status2.Text = "(Atmost 20 characters)";
-                status3.Text = "(Atmost 10 characters)";
+                status3.Text = "(Atmost 10)";
                 status4.Text = "(Retype password)";
                 return false;
             }
@@ -130,60 +110,21 @@ namespace MockSAP
             }
             return true;
         }
-               
-        private void Window_Closed(object sender, EventArgs e)
-        {
-            mainWindow.IsEnabled = true;
-        }
-                
-        private Boolean VerifyData(String uid, String upass, String upass_v)
-        {
-            if (uid.Length == 0 || upass.Length == 0 || upass_v.Length == 0)
-            {
-                status1.Foreground = new SolidColorBrush(Colors.Red);
-                status3.Foreground = new SolidColorBrush(Colors.Red);
-                status4.Foreground = new SolidColorBrush(Colors.Red);
-                status1.Text = "(Enter 10 or less digit number)";
-                status3.Text = "(Atmost 10 characters)";
-                status4.Text = "(Retype password)";
-                return false;
-            }
-            if (!IsDigitsOnly(uid))
-            {
-                status1.Foreground = new SolidColorBrush(Colors.Red);
-                status1.Text = "Uid contains characters other than numbers";
-                return false;
-            }
-            if (uid.Length > 10 && IsDigitsOnly(uid))
-            {
-                status1.Foreground = new SolidColorBrush(Colors.Red);
-                status1.Text = "Too Long";
-                return false;
-            }
-            if (!upass.Equals(upass_v))
-            {
-                status4.Foreground = new SolidColorBrush(Colors.Red);
-                status4.Text = "Passwords don't match";
-                return false;
-            }
-            if (upass.Equals(upass_v) && upass.Length > 10)
-            {
-                status3.Foreground = new SolidColorBrush(Colors.Red);
-                status3.Text = "Too Long";
-                return false;
-            }
-            return true;
-        }
 
         private bool IsDigitsOnly(String str)
         {
-            for (int i = 0; i < str.Length; i++)
+            for(int i=0;i<str.Length;i++)
             {
                 if (str[i] < '0' || str[i] > '9')
                     return false;
             }
 
             return true;
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            mainWindow.IsEnabled = true;
         }
     }
 }
